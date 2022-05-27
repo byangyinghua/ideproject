f47857e6a3e1995faa0473696068e729f1d28ba2
byangyinghua <shyangyinghua@outlook.com>
2022-05-27 16:29:55 +0800

    first commit

--- cms/src/main/java/bzl/common/Callback.java
+++
@@ -1,9 +0,0 @@
-package bzl.common;
-
-import com.alibaba.fastjson.JSONObject;
-
-public interface Callback {
-	   JSONObject callBack(String key,JSONObject valObj);
-	   
-}
-
--- cms/src/main/webapp/root_index.jsp
+++
@@ -1,2 +0,0 @@
-<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
- <%response.sendRedirect("/cms/WebRoot/login.html"); // 跳转到首页%>
